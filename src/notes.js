d0d9761b5529374f8f40883f4d822e096211148c
Muhammad Fajar <fajardwn7@gmail.com>
2022-12-15 16:28:38 +0700

    feature: crud notes

---
+++ src/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+  {
+    id: 'f3de34e0-69e1-451c-9669-a63f50bd4d9b',
+    title: 'Lorem Ipsum',
+    createdAt: '2022-12-14T16:40:19.776Z',
+    updatedAt: '2022-12-14T16:40:19.776Z',
+    tags: ['test', 'test2', 'test3'],
+    body: 'Repellendus voluptatum necessitatibus minus at et non.',
+  },
+  {
+    id: 'f9b70106-33df-477f-938a-e323d725921c',
+    title: 'vero dolores similique',
+    createdAt: '2022-12-14T16:40:19.776Z',
+    updatedAt: '2022-12-14T16:40:19.776Z',
+    tags: ['random', 'test2'],
+    body: 'Sed nihil non omnis voluptatem ipsam recusandae.',
+  },
+];
+
+module.exports = notes;
